18602046573ac5f237e203e280937b7853826ab6
Josef Hofer <me@jhofer.de>
2025-11-27 21:28:21 +0100

    chore: cleanup (#6)

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("bundle.css");
   eleventyConfig.addPassthroughCopy("./fonts/MapleMono-Regular.woff2");
   eleventyConfig.addPassthroughCopy("./assets");
+  eleventyConfig.setIncludesDirectory("templates");
 
   // Cache for playlist names to avoid repeated API calls
   const playlistCache = new Map();
